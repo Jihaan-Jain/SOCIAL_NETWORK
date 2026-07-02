70b90eb667a1c8dac56bda974a671731b5dc9860
Jihaan-Jain <146576474+Jihaan-Jain@users.noreply.github.com>
2026-07-02 12:39:13 +0530

    ass

--- a.java
+++ a.java
@@ -5,7 +5,7 @@ public class a {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Login Form"
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE
             frame.setSize(400, 220);
             frame.setLocationRelativeTo(null);
 
